5a8315d2a55c99f3209abd60199f04a7c228d102
Miguel Lugo <ingmchlugo@gmail.com>
2022-11-05 15:00:27 -0600

    FIX: config de base de datos y descomentar codigo

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Data;
 using System.Collections.Generic;
 using Limilabs.Client.IMAP;
 using Limilabs.Mail;
-using MySql.Data;
 using MySql.Data.MySqlClient;
 using Limilabs.Mail.Headers;
 using Limilabs.Mail.Licensing;
@@ -14,83 +12,69 @@ namespace mailConnection
     {
         static void Main(string[] args)
         {
-
             string fileName = Limilabs.Mail.Licensing.LicenseHelper.GetLicensePath();
             LicenseStatus status = Limilabs.Mail.Licensing.LicenseHelper.GetLicenseStatus();
-
-            Console.WriteLine(fileName);
 
             if (status != Limilabs.Mail.Licensing.LicenseStatus.Valid)
             {
                 throw new Exception("License is not valid: " + status);
 
-            } else
+            }
+            else
             {
                 Console.WriteLine("Licencia valida");
-            }
-        
-            
 
-        /*
-        string connectionString = "Server=127.0.0.1;Port=3306;Database=example_app;Uid=root;password=password;";
-        //MySqlConnection conexion = new MySqlConnection(connectionString);
-
-        try
-        {
-            MySqlConnection conn = new MySqlConnection();
-            conn.ConnectionString = connectionString;
-
-            Console.WriteLine("Connecting to MySQL...");
-            conn.Open();
-
-            MySqlCommand cmd = new MySqlCommand();
-
-            cmd.Connection = conn;
-
-
-
-            using (Imap imap = new Imap())
-            {
-                imap.ConnectSSL("open.cobama.com.mx");  // or ConnectSSL for SSL
-                imap.UseBestLogin("tickets@cobama.com.mx", "Mhtemplos2022+");
-                imap.SelectInbox();
-                List<long> uids = imap.Search(Flag.Unseen);
-                foreach (long uid in uids)
+                string connectionString = "Server=127.0.0.1;Port=3306;Database=SistemaTickets;Uid=user_sistema_tickets;password=tXDZD0VnznWLdZcB4QVN;";
+                //MySqlConnection conexion = new MySqlConnection(connectionString);
+                try
                 {
-                    IMail email = new MailBuilder().CreateFromEml(imap.GetMessageByUID(uid));
+                    MySqlConnection conn = new MySqlConnection();
+                    conn.ConnectionString = connectionString;
 
-                    cmd.CommandText = "INSERT INTO correos (asunto, enviado, mensaje, created_at) VALUES ( ?asunto, ?enviado, ?mensaje, ?created_at ) ";
-                    cmd.Parameters.Add("?asunto", MySqlDbType.VarChar).Value = email.Subject;
+                    Console.WriteLine("Connecting to MySQL...");
+                    conn.Open();
 
-                    foreach (MailBox m in email.From)
+                    MySqlCommand cmd = new MySqlCommand();
+
+                    cmd.Connection = conn;
+
+                    using (Imap imap = new Imap())
                     {
-                        Console.WriteLine(m.Address);
-                        Console.WriteLine(m.Name);
-                        cmd.Parameters.Add("?enviado", MySqlDbType.VarChar).Value = m.Name + " " + m.Address;
+                        imap.ConnectSSL("open.cobama.com.mx");  // or ConnectSSL for SSL
+                        imap.UseBestLogin("tickets@cobama.com.mx", "Mhtemplos2022+");
+                        imap.SelectInbox();
+                        List<long> uids = imap.Search(Flag.Unseen);
+                        foreach (long uid in uids)
+                        {
+                            IMail email = new MailBuilder().CreateFromEml(imap.GetMessageByUID(uid));
+
+                            cmd.CommandText = "INSERT INTO correos (asunto, enviado, mensaje, created_at) VALUES ( ?asunto, ?enviado, ?mensaje, ?created_at ) ";
+                            cmd.Parameters.Add("?asunto", MySqlDbType.VarChar).Value = email.Subject;
+
+                            foreach (MailBox m in email.From)
+                            {
+                                Console.WriteLine(m.Address);
+                                Console.WriteLine(m.Name);
+                                cmd.Parameters.Add("?enviado", MySqlDbType.VarChar).Value = m.Name + " " + m.Address;
+                            }
+
+                            cmd.Parameters.Add("?mensaje", MySqlDbType.VarChar).Value = email.Text;
+                            cmd.Parameters.Add("?created_at", MySqlDbType.DateTime).Value = email.Date;
+                            cmd.ExecuteNonQuery();
+
+                            Console.WriteLine(email.Subject);
+                            Console.WriteLine(email.From);
+                            Console.WriteLine(email.Text);
+                        }
+                        imap.Close();
                     }
-
-                    cmd.Parameters.Add("?mensaje", MySqlDbType.VarChar).Value = email.Text;
-                    cmd.Parameters.Add("?created_at", MySqlDbType.DateTime).Value = email.Date;
-                    cmd.ExecuteNonQuery();
-
-                    Console.WriteLine(email.Subject);
-                    Console.WriteLine(email.From);
-                    Console.WriteLine(email.Text);
+                    conn.Close();
                 }
-                imap.Close();
+                catch (MySql.Data.MySqlClient.MySqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-
-
-
-            conn.Close();
         }
-        catch (MySql.Data.MySqlClient.MySqlException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-        */
-
     }
-}
 }
